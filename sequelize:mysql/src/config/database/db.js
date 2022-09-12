5673057391566320480ac83a49e1da551ebeb94d
Adriana Latorre <101880897+AdrianaLatorreGit@users.noreply.github.com>
2022-09-11 23:04:32 -0300

    Add files via upload

--- sequelize:mysql/src/config/database/db.js
+++ sequelize:mysql/src/config/database/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 // Primeiro parimetro é o nome do banco, segundo username, terceiro senha do mysql
-const sequelize = new Sequelize("usuarios", "root", "***", {
+const sequelize = new Sequelize("usuarios", "root", "1234", {
     host: "localhost",
     dialect: "mysql",
 });
